89623c647a4fe4655ab3acbd391403a56605ebdc
Xincheng Huang <xh721@nyu.edu>
2020-02-26 02:52:24 -0500

    main functionality

---
+++ Assets/Scripts/M.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M : MonoBehaviour
+{
+    public int State = 1; //1: preparing, 2: working1, 3: working2, 4: working3, 5:done
+    private float start = 0;
+    Vector3 M2_POS;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        M2_POS = GameObject.Find("M_2").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("slots")[0].GetComponent<MeshRenderer>().enabled ||
+            GameObject.FindGameObjectsWithTag("slots")[1].GetComponent<MeshRenderer>().enabled ||
+            GameObject.FindGameObjectsWithTag("slots")[2].GetComponent<MeshRenderer>().enabled)
+        {
+            State = 1;
+        }
+        else if(State == 1)
+        {
+            State = 2;
+            this.GetComponent<MeshRenderer>().enabled = true;
+            string machine_name = GameObject.Find("slot1").GetComponent<slot>().catching;
+            Vector3 target = GameObject.Find(machine_name + "workspace").transform.position;
+            Vector3 dist = target - M2_POS;
+            this.transform.position = this.transform.position + dist;
+            M2_POS += dist;
+            start = Time.time;
+        }
+        if (State == 1)
+        {
+            this.GetComponent<MeshRenderer>().enabled = false;
+        }else if(State == 2)
+        {
+            if (Time.time - start >= 5)
+            {
+                State = 3;
+                this.GetComponent<MeshRenderer>().enabled = true;
+                string machine_name = GameObject.Find("slot2").GetComponent<slot>().catching;
+                Vector3 target = GameObject.Find(machine_name + "workspace").transform.position;
+                Vector3 dist = target - M2_POS;
+                this.transform.position = this.transform.position + dist;
+                M2_POS += dist;
+                start = Time.time;
+            }
+        }else if(State == 3)
+        {
+            if (Time.time - start >= 5)
+            {
+                State = 4;
+                this.GetComponent<MeshRenderer>().enabled = true;
+                string machine_name = GameObject.Find("slot3").GetComponent<slot>().catching;
+                Vector3 target = GameObject.Find(machine_name + "workspace").transform.position;
+                Vector3 dist = target - M2_POS;
+                this.transform.position = this.transform.position + dist;
+                M2_POS += dist;
+                start = Time.time;
+            }
+        }else if(State == 4)
+        {
+            if (Time.time - start >= 5)
+            {
+                State = 5;
+                Vector3 target = GameObject.Find("Done").transform.position;
+                Vector3 dist = target - M2_POS;
+                this.transform.position = this.transform.position + dist;
+                M2_POS += dist;
+                start = Time.time;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/slot.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slot : MonoBehaviour
+{
+    public string catching = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (catching == "" && other.GetComponent<Lock>() != null && !other.GetComponent<Lock>().isCatched)
+        {
+            Debug.Log("catched by " + this.name);
+            other.GetComponent<Lock>().isCatched = true;
+            catching = other.name;
+            this.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (catching != "" && other.name == catching)
+        {
+            GameObject machine = other.gameObject;
+            while (machine.transform.parent != null)
+            {
+                machine = machine.transform.parent.gameObject;
+            }
+            
+            if (machine.name != "Player")
+            {
+                machine.transform.position = Vector3.MoveTowards(machine.transform.position, new Vector3(this.transform.position.x, machine.transform.position.y, this.transform.position.z), 0.1f);
+                machine.transform.rotation = Quaternion.RotateTowards(machine.transform.rotation, this.transform.rotation, 2);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (catching != "" && other.GetComponent<Lock>() != null && other.GetComponent<Lock>().isCatched && other.name == catching)
+        {
+            other.GetComponent<Lock>().isCatched = false;
+            catching = "";
+            this.GetComponent<MeshRenderer>().enabled = true;
+        }
+
+    }
+}
