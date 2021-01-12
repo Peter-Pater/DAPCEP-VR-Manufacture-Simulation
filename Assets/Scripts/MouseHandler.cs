705e1e6bf8a60ea2922ed099dbcb643e78c816e2
Peter-Pater <xh721@nyu.edu>
2021-01-12 10:22:26 -0500

    player control preliminary

---
+++ Assets/Scripts/MouseHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHandler : MonoBehaviour
+{
+    public float horizontalSpeed = 3f;
+    public float verticalSpeed = 3f;
+    private float xRotation = 0.0f;
+    private float yRotation = 0.0f;
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
+        float mouseY = - Input.GetAxis("Mouse Y") * verticalSpeed;
+
+        yRotation += mouseX;
+        xRotation += mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        cam.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController characterController;
+    public float MovementSpeed = 2f;
+    public float Gravity = 9.8f;
+    private float velocity = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * this.MovementSpeed;
+        float vertical = Input.GetAxis("Vertical") * this.MovementSpeed;
+        characterController.Move((Vector3.right * horizontal + Vector3.forward * vertical) * Time.deltaTime);
+
+        if (characterController.isGrounded)
+        {
+            velocity = 0;
+        }
+        else
+        {
+            velocity -= Gravity * Time.deltaTime;
+            characterController.Move(new Vector3(0, velocity, 0));
+        }
+    }
+}
