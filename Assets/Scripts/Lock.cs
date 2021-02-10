88dab6732925751a76cc401dfc72a1d85dd5f317
Peter-Pater <xh721@nyu.edu>
2021-02-09 22:33:18 -0500

    prevent drop

--- Assets/Scripts/Lock.cs
+++ Assets/Scripts/Lock.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class Lock : MonoBehaviour
 {
     public bool isCatched = false;
+    private Vector3 myPosition;
+    private Quaternion myRotation;
+    private GameObject floor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.myPosition = this.gameObject.transform.position;
+        this.myRotation = this.gameObject.transform.rotation;
+        this.floor = GameObject.Find("Plane");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // prevent getting out of the world
+        if (this.gameObject.transform.position.y < (floor.transform.position - Vector3.down).y)
+        {
+            this.gameObject.transform.position = this.myPosition;
+            this.gameObject.transform.rotation = this.myRotation;
+        }
     }
 }
