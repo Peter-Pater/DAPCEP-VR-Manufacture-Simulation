9a1989a31fe5fc33ccda86e37046553e1f25f1b8
Peter-Pater <xh721@nyu.edu>
2021-02-09 22:33:04 -0500

    click grab or release

--- Assets/Scripts/GrabControl.cs
+++ Assets/Scripts/GrabControl.cs
@@ -6,22 +6,31 @@ public class GrabControl : MonoBehaviour
 {
     private Quaternion inHandRotation;
     private GameObject grabPoint;
+    private GameObject hand;
+    public bool handDetected;
+
+    public int grabState; // 0: nothing, 1: grabing, 2: releasing (not used)
+
+
     // Start is called before the first frame update
     void Start()
     {
-        grabPoint = GameObject.Find("GrabPoint");
-        inHandRotation = this.gameObject.transform.rotation;
+        this.grabPoint = GameObject.Find("GrabPoint");
+        this.hand = GameObject.Find("Hand");
+        this.inHandRotation = this.gameObject.transform.rotation;
+        this.grabState = 0;
+        this.handDetected = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.name == "Hand" && Input.GetMouseButton(0) && (GameObject.Find("Hand").GetComponent<Hand>().inHandObject == null || GameObject.Find("Hand").GetComponent<Hand>().inHandObject == this.gameObject))
+        if ((this.grabState == 0) && this.handDetected && Input.GetMouseButtonDown(0))
+        {
+            this.grabState = 1;
+            this.hand.GetComponent<Hand>().inHandObject = this.gameObject;
+        }
+        else if (this.grabState == 1)
         {
             if (this.gameObject.name == "SMALLBOT_A1P2_02" || this.gameObject.name == "SMALLBOT_A1P2_03")
             {
@@ -32,18 +41,33 @@ public class GrabControl : MonoBehaviour
                 this.gameObject.transform.position = grabPoint.transform.position;
             }
             this.gameObject.transform.rotation = this.inHandRotation;
-            if (GameObject.Find("Hand").GetComponent<Hand>().inHandObject == null)
+
+            if (Input.GetMouseButtonDown(0))
             {
-                GameObject.Find("Hand").GetComponent<Hand>().inHandObject = this.gameObject;
+                this.grabState = 0;
+                this.hand.GetComponent<Hand>().inHandObject = null;
             }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == this.hand && this.hand.GetComponent<Hand>().inHandObject == null)
+        {
+            this.handDetected = true;
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.name == "Hand" && GameObject.Find("Hand").GetComponent<Hand>().inHandObject == this.gameObject)
+        if (other.gameObject == this.hand)
         {
-            GameObject.Find("Hand").GetComponent<Hand>().inHandObject = null;
+            //if (this.grabState == 2 && this.hand.GetComponent<Hand>().inHandObject == this.gameObject)
+            //{
+            //    this.hand.GetComponent<Hand>().inHandObject = null;
+            //    this.grabState = 0;
+            //}
+            this.handDetected = false;
         }
     }
 }
